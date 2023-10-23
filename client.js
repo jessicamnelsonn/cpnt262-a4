3516e430acfb884ac56e400ace9e7fb337bd25dd
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-23 12:11:41 -0600

    added containers

--- client.js
+++ client.js
@@ -1,8 +1,13 @@
 // String
 const myString = "Jessica is the coolest";
-
+// Variable to add stuff to
+let strSplit = myString.split(' '); // Splits the string stored in the variable myString into an array of substrings using a space
+let longestWord = 0; // Initial value of 0
 
 //------------------------------------------------------------------------------------------//
 
 // String
 const myOriginalString = "This is my string reversed";
+// Variable to add stuff to
+let reversedString = '';
+
