843f5eaabd946d306c230024d664854d630ef921
Jessica Nelson <jessica.nelson60@gmail.com>
2023-10-23 12:06:50 -0600

    declared variables and set up html h2's and IDs

---
+++ client.js
@@ -0,0 +1,8 @@
+// String
+const myString = "Jessica is the coolest";
+
+
+//------------------------------------------------------------------------------------------//
+
+// String
+const myOriginalString = "This is my string reversed";
